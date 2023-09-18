8abb35790da3326a8572d161ba9638fd32577b0b
ameyukisora <64373235+ameyukisora@users.noreply.github.com>
2023-09-18 14:36:12 +0800

    update

--- .github/workflows/python/AdGuard.py
+++ .github/workflows/python/AdGuard.py
@@ -15,28 +15,31 @@ filtered_lines = [filter_line(line, "||", '^') for line in lines if filter_line(
 filtered_lines_whitelist = [filter_line(line, "@@||", '|^') for line in lines if filter_line(line, "@@||", '|^') is not None]
 
 # 创建文件
+def write_payload_to_file(file_path, content, filtered_lines):
+    with open(file_path, "w") as f:
+        f.write(content)
+        for line in filtered_lines:
+            f.write(f"  - '+.{line}'\n")
+
 output_dir = "autoupdate"
 os.makedirs(output_dir, exist_ok=True)
-output_file = os.path.join(output_dir, "AdGuard.yaml")
-output_file_whitelist = os.path.join(output_dir, "AdGuardWhitelist.yaml")
 
-payload_content = f'''payload:
-  # Blocklist of https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
-  # get {len(filtered_lines)} domain from AdGuard DNS filter
+file_data = {
+    "AdGuard.yaml": {
+        "description": "Blocklist of https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt",
+        "lines": filtered_lines,
+    },
+    "AdGuardWhitelist.yaml": {
+        "description": "Whitelist of https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt",
+        "lines": filtered_lines_whitelist,
+    },
+}
+
+for file_name, data in file_data.items():
+    file_path = os.path.join(output_dir, file_name)
+    payload_content = f'''payload:
+  # {data["description"]}
+  # get {len(data["lines"])} domain from AdGuard DNS filter
   # {lines[5][2:]}
 '''
-payload_content_whitelist = f'''payload:
-  # Whitelist of https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
-  # get {len(filtered_lines_whitelist)} domain from AdGuard DNS filter
-  # {lines[5][2:]}
-'''
-
-with open(output_file, "w") as f, open(output_file_whitelist, "w") as f_whitelist:
-    f.write(payload_content)
-    f_whitelist.write(payload_content_whitelist)
-
-    for line in filtered_lines:
-        f.write(f"  - '+.{line}'\n")
-
-    for line in filtered_lines_whitelist:
-        f_whitelist.write(f"  - '+.{line}'\n")
+    write_payload_to_file(file_path, payload_content, data["lines"])
