81e39a7640ac30de166d4e8bcbd34a1c17242ba3
ameyukisora <64373235+ameyukisora@users.noreply.github.com>
2023-09-20 10:02:24 +0800

    Update chnroutes2.py

--- .github/workflows/python/chnroutes2.py
+++ .github/workflows/python/chnroutes2.py
@@ -1,23 +1,25 @@
 import requests
 import os
 
-url = "https://raw.githubusercontent.com/misakaio/chnroutes2/master/chnroutes.txt"
-response = requests.get(url)
-lines = response.text.split("\n")
+def download_and_filter_ips(url, output_file):
+    # Download the file from the URL
+    response = requests.get(url)
+    lines = response.text.split("\n")
 
-# filter ip
-ip_list = [ip for ip in lines if ip and ip[0].isdigit()]
+    # Filter IP addresses
+    ip_list = [ip for ip in lines if ip and ip[0].isdigit()]
 
-# create file
-os.makedirs("autoupdate", exist_ok=True)
-output_file = "autoupdate/chnroutes.yaml"
-
-output_content = f'''payload:
+    # Write the IP addresses to the output YAML file
+    with open(output_file, "w") as f:
+        f.write(f'''payload:
   # https://github.com/misakaio/chnroutes2
   {lines[0]}
   {lines[1]}
-'''
+''')
+        f.writelines(f"  - '{ip}'\n" for ip in ip_list)
 
-with open(output_file, "w") as f:
-    f.write(output_content)
-    f.writelines(f"  - '{ip}'\n" for ip in ip_list)
+if __name__ == "__main__":
+    url = "https://raw.githubusercontent.com/misakaio/chnroutes2/master/chnroutes.txt"
+    os.makedirs("autoupdate", exist_ok=True)
+    output_file = "autoupdate/chnroutes.yaml"
+    download_and_filter_ips(url, output_file)
