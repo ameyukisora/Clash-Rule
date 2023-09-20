9ceed91a97be9e8ffe6c12dadd5004bf4c8d9abb
ameyukisora <64373235+ameyukisora@users.noreply.github.com>
2023-09-20 10:23:12 +0800

    Update Adblock.py

--- .github/workflows/python/Adblock.py
+++ .github/workflows/python/Adblock.py
@@ -4,38 +4,40 @@ from datetime import datetime
 import pytz
 
 
-def download_and_filter_lists(urls):
-    rules = []
+class AdblockListProcessor:
+    def __init__(self, urls, output_file):
+        self.list_urls = urls
+        self.output_yaml_file = output_file
+        self.lines_to_extract = []
 
-    for url in urls:
-        response = requests.get(url)
-        if response.status_code == 200:
-            lines = response.text.splitlines()
-            rules.extend([line.strip() for line in lines if line.strip().startswith(("DOMAIN", "IP-CIDR"))])
-        else:
-            print(f"无法下载文件：{url}")
+    def download_lists(self):
+        for url in self.list_urls:
+            response = requests.get(url)
+            if response.status_code == 200:
+                lines = response.text.splitlines()
+                self.lines_to_extract.extend(
+                    [line.strip() for line in lines if line.strip().startswith(("DOMAIN", "IP-CIDR"))])
+            else:
+                print(f"无法下载文件：{url}")
 
-    return rules
+    def write_to_yaml_file(self):
+        line_counts = {}
+        for line in self.lines_to_extract:
+            line_type = line.split(",")[0]
+            line_counts[line_type] = line_counts.get(line_type, 0) + 1
 
+        tz = pytz.timezone('Asia/Shanghai')
+        current_time = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
 
-def write_to_yaml_file(rules, output_file):
-    line_counts = {}
-    for line in rules:
-        line_type = line.split(",")[0]
-        line_counts[line_type] = line_counts.get(line_type, 0) + 1
-
-    tz = pytz.timezone('Asia/Shanghai')
-    current_time = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
-
-    with open(output_file, "w") as output:
-        output.write(f'''payload:
+        with open(self.output_yaml_file, "w") as output:
+            output.write(f'''payload:
   # Merged BanAD, BanProgramAD, BanEasyListChina from https://github.com/ACL4SSR/ACL4SSR/tree/master/Clash
   # {current_time}
   # {", ".join(f'{k}: {v}' for k, v in line_counts.items())}
   # TOTAL: {sum(line_counts.values())}
 ''')
-        for line in rules:
-            output.write(f"  - {line}\n")
+            for line in self.lines_to_extract:
+                output.write(f"  - {line}\n")
 
 
 if __name__ == "__main__":
@@ -48,5 +50,6 @@ if __name__ == "__main__":
     os.makedirs("autoupdate", exist_ok=True)
     output_yaml_file = "autoupdate/Adblock.yaml"
 
-    lines_to_extract = download_and_filter_lists(list_urls)
-    write_to_yaml_file(lines_to_extract, output_yaml_file)
+    adblock_processor = AdblockListProcessor(list_urls, output_yaml_file)
+    adblock_processor.download_lists()
+    adblock_processor.write_to_yaml_file()
