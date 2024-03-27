c00f7b1f865790c205045e80f268ec0e4fccafb8
ameyukisora <64373235+ameyukisora@users.noreply.github.com>
2024-03-27 11:04:51 +0800

    update

--- .github/workflows/python/Proxy_urls.py
+++ .github/workflows/python/Proxy_urls.py
@@ -5,8 +5,7 @@ from pytz import timezone
 response = requests.get("https://raw.githubusercontent.com/Loyalsoldier/clash-rules/release/gfw.txt")
 urls = response.text.splitlines()[1:]
 
-now = datetime.now(timezone('UTC'))
-timestamp = now.astimezone(timezone('Asia/Shanghai')).strftime("%Y-%m-%d %H:%M:%S")
+timestamp = datetime.now(pytz.timezone('Asia/Shanghai')).strftime("%Y-%m-%d %H:%M:%S")
 
 with open("autoupdate/Proxy_urls.yaml", "w") as f:
     f.write(f'''payload:
