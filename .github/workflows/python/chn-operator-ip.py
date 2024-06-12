91ab652465e0d62eb435c6b1e9e7d104db375dea
ameyukisora <64373235+ameyukisora@users.noreply.github.com>
2024-06-12 20:50:28 +0800

    add ipv6

--- .github/workflows/python/chn-operator-ip.py
+++ .github/workflows/python/chn-operator-ip.py
@@ -5,6 +5,9 @@ import pytz
 response = requests.get("https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt")
 ips = [ip for ip in response.text.splitlines() if ip and ip[0].isdigit()]
 
+response6 = requests.get("https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china6.txt")
+ips6 = [ip6 for ip6 in response6.text.splitlines() if ip6 and ip6[0].isalnum()]
+
 timestamp = datetime.now(pytz.timezone('Asia/Shanghai')).strftime("%Y-%m-%d %H:%M:%S")
 
 with open("autoupdate/cnipcidr.yaml", "w") as f:
@@ -13,4 +16,12 @@ with open("autoupdate/cnipcidr.yaml", "w") as f:
   # {timestamp}
   # TOTAL: {len(ips)}
 {'\n'.join(f"  - '{ip}'" for ip in ips)}
+''')
+
+with open("autoupdate/cnipcidr6.yaml", "w") as f:
+    f.write(f'''payload:
+  # https://github.com/gaoyifan/china-operator-ip
+  # {timestamp}
+  # TOTAL: {len(ips6)}
+{'\n'.join(f"  - '{ip6}'" for ip6 in ips6)}
 ''')
