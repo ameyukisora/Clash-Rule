de38e7d99faad85ac460a260b087f3a0c457613c
ameyukisora <64373235+ameyukisora@users.noreply.github.com>
2024-09-11 12:21:10 +0800

    cnip

--- .github/workflows/python/chn-operator-ip.py
+++ .github/workflows/python/chn-operator-ip.py
@@ -10,7 +10,7 @@ ips6 = [ip6 for ip6 in response6.text.splitlines() if ip6 and ip6[0].isalnum()]
 
 timestamp = datetime.now(pytz.timezone('Asia/Shanghai')).strftime("%Y-%m-%d %H:%M:%S")
 
-with open("autoupdate/cnipcidr.yaml", "w") as f:
+with open("autoupdate/cn.yaml", "w") as f:
     f.write(f'''payload:
   # https://github.com/gaoyifan/china-operator-ip
   # {timestamp}
@@ -18,7 +18,7 @@ with open("autoupdate/cnipcidr.yaml", "w") as f:
 {'\n'.join(f"  - '{ip}'" for ip in ips)}
 ''')
 
-with open("autoupdate/cnipcidr6.yaml", "w") as f:
+with open("autoupdate/cn_v6.yaml", "w") as f:
     f.write(f'''payload:
   # https://github.com/gaoyifan/china-operator-ip
   # {timestamp}
